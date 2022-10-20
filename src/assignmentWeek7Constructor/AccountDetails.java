707446f5e350a9985365552141730745b64625da
Parul4214 <parul.parul4291@gmail.com>
2022-10-20 00:18:49 -0400

    optional ques

--- src/assignmentWeek7Constructor/AccountDetails.java
+++ src/assignmentWeek7Constructor/AccountDetails.java
@@ -5,22 +5,58 @@ public class AccountDetails {
 	int account_number;
 	int account_PIN;
 	double account_balance;
-	double withdraw;
-	double deposit;
 	
 	public AccountDetails(int numberOnAccount, int PINOnAccount, double balanceOfAccount) {
 		
 		
-		account_number=numberOnAccount;
-		account_PIN=PINOnAccount;
-		account_balance= balanceOfAccount;
+//		account_number=numberOnAccount;
+//		account_PIN=PINOnAccount;
+//		account_balance= balanceOfAccount;
+		this.account_number = numberOnAccount;
+        this.account_PIN = PINOnAccount;
+        this.account_balance = balanceOfAccount;
+    
 		
 		
 	}
 	
-	int credential_check(int acc_num, int acc_pin) {
-		
-		
+
+	public void deposit_money(double deposit_money_requested) {
+		this.account_balance=this.account_balance+deposit_money_requested;
+		System.out.println("Deposit Successful, Account Balance: " +this.account_balance);
 	}
+	
+	public double get_balance() {
+		return this.account_balance;
+	}
+	
+	public void withdraw_money(double withdraw_money_requested) {
+		this.account_balance=this.account_balance-withdraw_money_requested;
+		System.out.println(" Withdraw Successful, Account Balance: " +this.account_balance);
+	}
+
+	public int credential_check(int acc_num, int acc_pin) {
+		if (acc_num==this.account_number && acc_pin==this.account_PIN)
+			
+		return 1;
+		else 
+			return 0;
+	}
+
+	
+//	double withdraw(double withdrawAmount) {
+//        if (withdrawAmount > 0) {
+//            this.balance = this.balance - withdrawAmount;
+//            return this.balance;
+//        }
+//        
+//        else return 0;}
+//	 double deposit(double depositAmount) {
+//	        if (depositAmount > 0) {
+//	            this.balance = this.balance + depositAmount;
+//	            return this.balance;
+//	        }
+//	        else return 0;
+//	    }
 
 }
--- src/assignmentWeek7Constructor/AccountDetailsMain.java
+++ src/assignmentWeek7Constructor/AccountDetailsMain.java
@@ -5,38 +5,75 @@ import java.util.Scanner;
 public class AccountDetailsMain {
 
 	public static void main(String[] args) {
+
+		// AccountDetails Obj = new AccountDetails();
+
 		Scanner sc = new Scanner(System.in);
 
-		AccountDetails accountDetailsUser1 = new AccountDetails(74107410, 1234, 250.99);
-		AccountDetails accountDetailsUser2 = new AccountDetails(85208520, 4567, 300.99);
-		AccountDetails accountDetailsUser3 = new AccountDetails(96309630, 8910, 450.99);
-		AccountDetails accountDetailsUser4 = new AccountDetails(75307530, 1112, 500.99);
-		AccountDetails accountDetailsUser5 = new AccountDetails(96309630, 1314, 650.99);
+		AccountDetails accountDetailsUser1 = new AccountDetails(101, 1111, 250.99);
+		AccountDetails accountDetailsUser2 = new AccountDetails(202, 2222, 300.99);
+		AccountDetails accountDetailsUser3 = new AccountDetails(303, 3333, 450.99);
+		AccountDetails accountDetailsUser4 = new AccountDetails(404, 4444, 500.99);
+		AccountDetails accountDetailsUser5 = new AccountDetails(505, 5555, 650.99);
 
 		AccountDetails[] allAccountsDatabase = { accountDetailsUser1, accountDetailsUser2, accountDetailsUser3,
 				accountDetailsUser4, accountDetailsUser5 };
 		// The program should prompt the user to first enter the account number and PIN
-		System.out.println("Please enter you Account Number");
-		int entered_acc_num = sc.nextInt();
+		boolean is_account_verified = false;
+		int acc_selector = 10; // initialized to fasle value
+		while (!is_account_verified) {
 
-		System.out.println("Please enter you PIN");
-		int entered_PIN = sc.nextInt();
+			System.out.println("Please enter you Account Number");
+			int entered_acc_num = sc.nextInt();
 
-		AccountDetails Obj = new AccountDetails(entered_PIN, entered_PIN, entered_PIN);
-		
-		
-		System.out.println("Credential check :" );
-		for (int i = 0; i < allAccountsDatabase.length; i++)
-			if (entered_acc_num == allAccountsDatabase[i].account_number
-					&& entered_PIN == allAccountsDatabase[i].account_PIN) {
+			System.out.println("Please enter you PIN");
+			int entered_PIN = sc.nextInt();
 
-				System.out.println("You Account balance is " + allAccountsDatabase[i].account_balance);
-				System.out.println("Press\n" + "1: To Withdraw\n" + "2: To Deposit");
-				int entered_input=sc.nextInt();
-				if(entered_input)
-				System.out.println("Please enter ");
+			for (int i = 0; i < allAccountsDatabase.length; i++)
+				if (allAccountsDatabase[i].credential_check(entered_acc_num, entered_PIN) == 1) {
+					is_account_verified = true;
+					acc_selector = i;
+				}
+			if (!is_account_verified)
+				System.out.println("Credential Mismatch, Enter valid account number and PIN");
+
+		}
+
+		System.out.println("You Account balance is " + allAccountsDatabase[acc_selector].account_balance);
+		System.out.println("Press\n" + "1: To Withdraw\n" + "2: To Deposit");
+		int entered_input = sc.nextInt();
+		boolean user_abort = false;
+
+		if (entered_input == 1) {
+			while (!user_abort) {
+				System.out.println("How much money do you want to withdraw?");
+				double withdraw_request = sc.nextDouble();
+
+				if (withdraw_request > allAccountsDatabase[acc_selector].get_balance())
+				{
+					System.out.println("Insufficient funds, Requested amount exceeded available funds");
+					System.out.println("Do you wish to try again?\n"
+							+ "1: Try Again\n"
+							+ "2: Abort");
+					int abort_request = sc.nextInt();
+					if (abort_request==2)
+						user_abort=true;
+				}
+				else {
+					allAccountsDatabase[acc_selector].withdraw_money(withdraw_request);
+					break;
+				}
 			}
+		}
+
+		else if (entered_input == 2) {
+			System.out.println("How much money do you want to deposit?");
+			double deposit_request = sc.nextDouble();
+			allAccountsDatabase[acc_selector].deposit_money(deposit_request);
+		}
 
 	}
+//				System.out.println("Please enter ");
+//			}
 
 }
